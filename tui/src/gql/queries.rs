0149b147e43cb138763aedf9f237aa545eb525cc
Andre Mermegas <andmer@gmail.com>
2026-05-05 15:51:35 -0400

    feat(tui): codegen for sessions, cost, and agents GraphQL queries

--- tui/src/gql/queries.rs
+++ tui/src/gql/queries.rs
@@ -27,3 +27,99 @@ pub struct RealtimeFeed;
     response_derives = "Debug, Clone"
 )]
 pub struct GatewayStatus;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/sessions.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct Sessions;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/session_detail.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct SessionDetail;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/turn.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct Turn;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/spend_by_provider.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct SpendByProvider;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/spend_by_model.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct SpendByModel;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/spend_by_framework.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct SpendByFramework;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/daily_spend.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct DailySpend;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/usage_summary.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct UsageSummary;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/agent_summary.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct AgentSummary;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/agent_framework_distribution.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct AgentFrameworkDistribution;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/top_developers.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct TopDevelopers;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "graphql/schema.graphql",
+    query_path = "graphql/top_repositories.graphql",
+    response_derives = "Debug, Clone"
+)]
+pub struct TopRepositories;
